b5f6eac02ec11edb4a4c91fc281d8e02ffd866bc
natrix_dev <discordlenko@gmail.com>
2022-10-21 17:12:36 +0200

    Create unblocker.py

---
+++ unblocker.py
@@ -0,0 +1,31 @@
+import json 
+import discord 
+import pythonconsole
+import os64 & os32
+
+def __main__:
+  if main=0:
+    try:
+      get discord()
+      config.ids.import as * 
+      from ids get userid & blockerid 
+      try:
+        userid.token.auth.connectByemail()=True
+        try:
+          groups.servers.True=0
+          try:
+            connectTo (blocker_acc) 
+            if true(): 
+              false.depriss=1
+              faille.exploit(=8999)$
+            if token = F;
+            return token = T;
+          
+        try: 
+          unblocking()
+          
+          
+          else : 
+            print('Unblocked with success")
+                  return;:
+                    print('Failed to unblock')
